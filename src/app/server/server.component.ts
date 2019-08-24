717e6c463bd81c319242d281263aa822ce05aece
Soulyana <soulyana.lakew@accenturefederal.com>
2019-08-23 22:04:29 -0400

    styling elements dynamically with ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,21 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css']
 })
-export class ServerComponent implements OnInit {
-  serverId = 10;
-  serverStatus = 'offline';
+export class ServerComponent {
+  // tslint:disable-next-line: no-inferrable-types
+  serverId: number = 10;
+  // tslint:disable-next-line: no-inferrable-types
+  serverStatus: string = 'offline';
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
   }
 
   getServerStatus() {
     return this.serverStatus;
   }
 
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
 }
