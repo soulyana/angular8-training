d9d3fd77b76792831fece7b918e982331e4ad6a9
Soulyana <soulyana.lakew@accenturefederal.com>
2019-08-23 21:42:12 -0400

    using  ngIf to output data conditionaly

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Test Server';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -20,10 +21,11 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
-  onUpdateServerName(event: any) {
+  onUpdateServerName(event: Event) {
     // console.log(event);
     this.serverName = (<HTMLInputElement>event.target).value;
   }
